4eaf0d7c8bc5aef0570c512c9a23b7e55eb07163
Andrey Postupalenko <andrey.postupalenko@saltpepper.ru>
2020-08-23 17:35:30 +0300

    Add UserTest model, add scope for Test, add tests_by_level method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,13 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
+  has_many :user_tests
+  has_many :users, through: :user_tests
+
+  scope :names_by_category, (lambda do |category|
+    where(categories: { title: category })
+    .order(id: :desc)
+    .includes(:category)
+    .pluck(:title)
+  end)
 end
