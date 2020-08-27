1f33adad4ebdfa66e4e761150fb56900022392fc
Andrey Postupalenko <andrey.postupalenko@saltpepper.ru>
2020-08-27 09:48:31 +0300

    Add scopes and validations

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,14 +5,16 @@ class Test < ApplicationRecord
   has_many :user_tests
   has_many :users, through: :user_tests
 
-  # scope :names_by_category, (lambda do |category|
-  #   where(categories: { title: category })
-  #   .order(id: :desc)
-  #   .includes(:category)
-  #   .pluck(:title)
-  # end)
+  validates :title, presence: true
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, uniqueness: { scope: :level, message: 'should be uniq' }
+
+  scope :by_category, ->(category) { includes(:category).where(categories: { title: category }) }
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: (5..)) }
 
   def self.names_by_category(category)
-    includes(:category).where(categories: { title: category }).order(id: :desc).pluck(:title)
+    by_category(category).order(id: :desc).pluck(:title)
   end
 end
