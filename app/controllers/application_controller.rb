74ef3032d9db57aca22880d0fe9aa17dd43ac7cf
Andrey Postupalenko <andrey.postupalenko@saltpepper.ru>
2020-09-02 19:55:40 +0300

    Add resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_record_not_found
+
+  private
+
+  def rescue_with_record_not_found
+    render plain: 'Record Not Found!'
+  end
 end
