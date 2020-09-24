27f6811a6f7695aaf4778915d88eb1c043dd25b7
Andrey Postupalenko <andrey.postupalenko@saltpepper.ru>
2020-09-24 16:02:48 +0300

    Add Authentication

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,6 @@
 class TestsController < ApplicationController
-  before_action :set_test, only: %i[show edit update destroy start]
-  before_action :set_user, only: :start
+  before_action :authenticate_user!
+  before_action :set_test, only: %i[show start]
 
   def index
     @tests = Test.unscoped
@@ -9,17 +9,13 @@ class TestsController < ApplicationController
   def show; end
 
   def start
-    @user.tests << @test
-    redirect_to @user.test_passage(@test)
+    current_user.tests << @test
+    redirect_to current_user.test_passage(@test)
   end
 
   private
 
   def set_test
     @test = Test.find(params[:id])
-  end
-
-  def set_user
-    @user = User.first
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def link_to_github(author, repo)
     link_to 'Project: TestGuru,', "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def flash_message(type)
+    content_tag :p, flash[type], class: "flash #{type}" if flash[type]
+  end
 end
